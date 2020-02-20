d6c4746e0c0073d8cf0ffbcf6b3c533eb43c4376
James Houghton <jhoughton@virginia.edu>
2020-02-20 15:54:42 -0500

    Read frames with ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,57 @@
 use std::env;
+use std::result::Result;
 
 extern crate ffmpeg_dev;
 
 use ffmpeg_dev::sys::{
     avformat_open_input,
+    avformat_close_input,
     avformat_find_stream_info,
     av_dump_format,
+    avcodec_register_all,
+    avcodec_alloc_context3,
+    avcodec_free_context,
+    avcodec_parameters_to_context,
+    avcodec_find_decoder,
+    avcodec_open2,
+    avcodec_close,
     AVFormatContext,
+    AVCodecContext,
+    AVStream,
+};
+
+use ffmpeg_dev::sys::{
+    AVMediaType_AVMEDIA_TYPE_VIDEO as AVMEDIA_TYPE_VIDEO,
 };
 
 mod process;
+
+fn get_video_stream<'a>(streams: &'a Vec<&AVStream>) -> Result<(&'a AVStream, usize), &'static str> {
+    unsafe {
+        for (index, stream_ptr) in streams.iter().enumerate() {
+            let codecpar = *stream_ptr.codecpar;
+            if codecpar.codec_type == AVMEDIA_TYPE_VIDEO {
+                println!("found video stream at index {}", index);
+                return Ok((streams[index], index));
+            }
+        }
+    }
+
+    return Err("no video stream found");
+}
+
+fn get_streams(ifmt_ctx: &AVFormatContext) -> Vec<&AVStream> {
+    let nb_streams = (*ifmt_ctx).nb_streams as usize;
+    unsafe {
+        let streams = std::slice::from_raw_parts((*ifmt_ctx).streams, nb_streams)
+            .iter()
+            .map(|x| (*x).as_ref().expect("not null"))
+            .collect::<Vec<&AVStream>>();
+
+        streams
+    }
+
+}
 
 fn main() {
     println!("starting timer");
@@ -29,26 +71,75 @@ fn main() {
         Err(e) => panic!(e),
     };
 
-    let mut ifmt_ctx: *mut AVFormatContext = std::ptr::null_mut();
-
-    unsafe { // check video format
-        avformat_open_input(&mut ifmt_ctx,
+    let mut ifmt_ctx_ptr: *mut AVFormatContext = unsafe {
+        let mut ifmt_ctx_ptr: *mut AVFormatContext = std::ptr::null_mut();
+        avformat_open_input(&mut ifmt_ctx_ptr,
                             input_path_cstr.as_ptr(),
                             std::ptr::null_mut(),
                             std::ptr::null_mut());
 
-        avformat_find_stream_info(ifmt_ctx, std::ptr::null_mut());
+        if ifmt_ctx_ptr == std::ptr::null_mut() {
+            eprintln!("failed to get format context");
+            std::process::exit(-1);
+        }
 
-        av_dump_format(ifmt_ctx,
+        avformat_find_stream_info(ifmt_ctx_ptr, std::ptr::null_mut());
+
+        av_dump_format(ifmt_ctx_ptr,
                        0,
                        input_path_cstr.as_ptr(),
                        0);
+
+        ifmt_ctx_ptr
+    };
+
+    let mut ifmt_ctx: &mut AVFormatContext = unsafe { &mut *ifmt_ctx_ptr };
+
+    let streams: Vec<&AVStream> = get_streams(&ifmt_ctx);
+
+    let (video_stream, video_stream_idx) = match get_video_stream(&streams) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(-1);
+        }
+    };
+
+    let mut codec_ctx_ptr: *mut AVCodecContext = unsafe {
+        avcodec_register_all();
+
+        let codec_ctx_ptr = avcodec_alloc_context3(std::ptr::null_mut());
+        avcodec_parameters_to_context(codec_ctx_ptr, video_stream.codecpar);
+        if codec_ctx_ptr == std::ptr::null_mut() {
+            eprintln!("failed to get codec from codec parameters");
+            std::process::exit(-1);
+        }
+
+        println!("got codec parameters");
+
+        let codec_ptr = avcodec_find_decoder((*codec_ctx_ptr).codec_id);
+        if codec_ptr == std::ptr::null_mut() {
+            eprintln!("codec not supported: id:{}", (*codec_ctx_ptr).codec_id);
+            std::process::exit(-1);
+        }
+
+        let codec = *codec_ptr;
+
+        avcodec_open2(codec_ctx_ptr, &codec, std::ptr::null_mut());
+
+        codec_ctx_ptr
+    };
+
+    let mut codec_ctx: &mut AVCodecContext = unsafe { &mut *codec_ctx_ptr };
+
+    let intervals: Vec<(u32, u32)> = process::find_white_frame_intervals(&mut ifmt_ctx, &mut codec_ctx, video_stream_idx);
+
+    unsafe {
+        avcodec_close(codec_ctx_ptr);
+        avformat_close_input(&mut ifmt_ctx_ptr);
+        avcodec_free_context(&mut codec_ctx_ptr);
     }
 
-    #[allow(unused_unsafe)]
-    unsafe { // check video codec
-    }
-
-    process::find_white_frame_intervals();
+    println!("done");
 
 }
--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,114 @@
-pub fn find_white_frame_intervals() {
-    println!("find_white_frame_intervals");
+extern crate ffmpeg_dev;
+
+use std::result::Result;
+
+use ffmpeg_dev::sys::{
+    av_read_frame,
+    av_init_packet,
+    av_packet_unref,
+    avcodec_send_packet,
+    avcodec_receive_frame,
+    av_frame_alloc,
+    av_frame_free,
+    av_frame_unref,
+    AVFrame,
+    AVPacket,
+    AVCodecContext,
+    AVFormatContext,
+    EOF,
+    EAGAIN,
+};
+
+use ffmpeg_dev::extra::defs::{
+    averror
+};
+
+static AV_EAGAIN: i32 = EAGAIN as i32;
+
+fn make_av_packet() -> AVPacket {
+    let mut packet = AVPacket {
+        buf: std::ptr::null_mut(),
+        pts: 0,
+        dts: 0,
+        data: std::ptr::null_mut(),
+        size: 0,
+        stream_index: 0,
+        flags: 0,
+        side_data: std::ptr::null_mut(),
+        side_data_elems: 0,
+        duration: 0,
+        pos: 0,
+        convergence_duration: 0
+    };
+
+    unsafe { av_init_packet(&mut packet); }
+
+    packet
+}
+
+fn process_frame(codec_ctx: &mut AVCodecContext, packet: &mut AVPacket, frame: &mut AVFrame) -> Result<bool, (&'static str, i32)> {
+    let mut ret: i32;
+    if {
+        unsafe { ret = avcodec_send_packet(codec_ctx, packet) };
+        ret != 0
+    } {
+        return Err(("avcodec_send_packet", ret));
+    }
+
+    while {
+        ret = unsafe { avcodec_receive_frame(codec_ctx, frame) };
+        if ret != 0 && ret != EOF && ret != unsafe { averror(AV_EAGAIN) } {
+            return Err(("avcodec_receive_frame", ret));
+        }
+        ret == AV_EAGAIN
+    } { }
+
+
+    unsafe {
+        av_frame_unref(frame);
+    }
+
+    return Ok(false);
+}
+
+pub fn find_white_frame_intervals(fmt_ctx: &mut AVFormatContext, mut codec_ctx: &mut AVCodecContext, video_stream_idx: usize) -> Vec<(u32, u32)> {
+    println!("searching for white frame intervals");
+
+    let mut vec = Vec::new();
+
+    let mut packet: AVPacket = make_av_packet();
+
+    let mut av_frame_ptr: *mut AVFrame = unsafe { av_frame_alloc() };
+    let mut av_frame: &mut AVFrame = unsafe { &mut *av_frame_ptr };
+
+    println!("initialized packet");
+
+    let mut frame: u32 = 0;
+    while unsafe { av_read_frame(fmt_ctx, &mut packet) } >= 0
+    {
+        if video_stream_idx as i32 != packet.stream_index {
+            continue;
+        }
+
+        print!("\rProcessing frame: {}", frame);
+        frame += 1;
+
+        let white: bool = match process_frame(&mut codec_ctx, &mut packet, &mut av_frame) {
+            Err((f, c)) => panic!("unhandled error returned from {}: code {}", f, c),
+            Ok(b) => b,
+        };
+
+        unsafe {
+            av_packet_unref(&mut packet);
+        }
+    }
+    println!();
+
+    unsafe {
+        av_frame_free(&mut av_frame_ptr);
+    }
+
+    println!("done with frames");
+
+    return vec;
 }
